4bc476e1e9c5548c8841ac4771b15d27715318bd
Taylor Ng <taylormng95@gmail.com>
2024-06-24 10:42:29 -0400

    completes badges lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,23 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
 def assign_rooms(names):
-    return None
+    room_number = 1
+    lst = []
+    for name in names:
+        lst.append(f'Hello, {name}! You\'ll be assigned to room {room_number}!')
+        room_number += 1
+    return lst
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+    for badge in badges:
+        print(badge)
+    for room in rooms:
+        print(room)
+
+printer(['Taylor', 'Nancy'])
